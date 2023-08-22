d1e63f5c8c00597ee9fe3210c4bb5bf821e585b8
jgardona <jcbritobr@gmail.com>
2023-08-22 10:38:43 -0300

    integration tests finnished

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # leetfy
+Leetfy encodes and transforms your text in leet using low frequency leet alphabet, and full leet alphabet.
+Low frequency leet is good for passwords, as you aren't using a full leet dialect, and it can enhance your passwords quality.
+
+* **Install**
+
+```
+cargo install --git https://github.com/jcbritobr/leetfy
+```
+
+* **Test**
+
+Download or clone the source code.
+```
+cargo test
+```
+
+* **Use**
+```
+$ leetfy --help
+
+Leetfy your texts
+
+Usage: leetfy --filename <FILENAME> <MODE>
+
+Arguments:
+  <MODE>  [possible values: low, full]
+
+Options:
+  -f, --filename <FILENAME>  
+  -h, --help                 Print help
+  -V, --version              Print version
+```
--- src/cli.rs
+++ src/cli.rs
@@ -35,9 +35,9 @@ pub fn execute_cli() -> Result<()> {
 
 #[cfg(test)]
 mod tests {
-    use anyhow::{Ok, Result};
-
     use super::read_full_file;
+    use anyhow::{Context, Ok, Result};
+    use assert_cmd::Command;
 
     #[test]
     fn it_works() {}
@@ -48,5 +48,35 @@ mod tests {
         let data = read_full_file("tests/text.txt")?;
         assert_eq!(text, data);
         Ok(())
+    }
+
+    #[test]
+    fn test_cmd_leetfy_low_frequency() {
+        let mut cmd = Command::cargo_bin("leetfy")
+            .context("we need assert that the binary leetfy is available")
+            .unwrap();
+        let test = cmd
+            .arg("--filename")
+            .arg("tests/text.txt")
+            .arg("low")
+            .assert();
+        test.success().stdout(predicates::str::contains(
+            "th3 qu1ck 8r0wn f0x jump5 0v3r th3 l4zy d0g",
+        ));
+    }
+
+    #[test]
+    fn test_cmd_leetfy_full_frequency() {
+        let mut cmd = Command::cargo_bin("leetfy")
+            .context("we need assert that the binary leetfy is available")
+            .unwrap();
+        let test = cmd
+            .arg("--filename")
+            .arg("tests/text.txt")
+            .arg("full")
+            .assert();
+        test.success().stdout(predicates::str::contains(
+            "+#3 9v1[x 820w~ f0* ]vm?5 0v32 +#3 142j )06",
+        ));
     }
 }
